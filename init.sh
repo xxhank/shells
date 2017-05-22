8112dfd7d849a366700c81914ca0d30287c74b1f
王超9 <wangchao9@le.com>
2017-05-22 11:24:10 +0800

    初始化项目
    1.创建uncrustify.cfg连接文件

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# 初始化项目
+#
+#
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly script_path="${BASH_SOURCE[0]}"
+readonly script_dir="$( cd "$( dirname "${script_path}" )" && pwd )"
+
+SOURCE="$script_dir/../codestyle/uncrustify.cfg"
+TARGET="$script_dir/../uncrustify.cfg"
+
+if [[ -e "$TARGET" ]]; then
+    if [[ -L "$TARGET" ]]; then
+        rm -rf "$TARGET"
+        ln -s "$SOURCE" "$TARGET"
+    else
+        echo "$TARGET is not a symbol link"
+    fi
+else
+    ln -s "$SOURCE" "$TARGET"
+fi
