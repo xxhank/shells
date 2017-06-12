0dfb008fad3874e24687db664632393ad48b0f77
王超9 <wangchao9@le.com>
2017-06-12 15:29:29 +0800

    使用infer分析代码

--- infer.sh
+++ infer.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-pushd "$1"
+ROOTDIR="$1"
+CLEAN="${2:-""}"
+WORKSPACE="Le123PhoneClient"
+SCHEME="sdsp"
+pushd "$ROOTDIR"
 
-xcodebuild -workspace Le123PhoneClient.xcworkspace -scheme sdsp -configuration Debug -sdk iphonesimulator clean 2>&1 | grep -v "Plug-ins"
-infer -- xcodebuild -workspace Le123PhoneClient.xcworkspace -scheme sdsp -configuration Debug -sdk iphonesimulator 2>&1 | grep -v "Plug-ins"
+XCODEBUILD="xcodebuild -workspace $WORKSPACE.xcworkspace -scheme $SCHEME -configuration Debug -sdk iphoneos"
+if [[ "$CLEAN" == "--clean" ]]; then
+    $XCODEBUILD clean 2>&1 | grep -v "Plug-ins"
+fi
+
+# shellcheck disable=
+infer run --reactive \
+ --infer-blacklist-path-regex "Pods/.*"\
+ --infer-blacklist-path-regex "lib/.*"\
+ --infer-blacklist-path-regex "Tests/.*"\
+ --infer-blacklist-path-regex ".*\.framework/.*"\
+ -- $XCODEBUILD build 2>&1 | grep -v "Plug-ins"
 popd
+
+inferTraceBugs --html
+open "./infer-out/report.html/index.html"
