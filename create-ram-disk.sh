109a57ca0bfd7f91c6341caacc6528b3f7d7e5de
王超9 <wangchao9@le.com>
2017-06-14 12:17:07 +0800

    扩大RAMDISK大小

--- create-ram-disk.sh
+++ create-ram-disk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 RAMDISK="XCode Boost"
-SIZE=2048 #size in MB for ramdisk.
+SIZE=4096 #size in MB for ramdisk.
 diskutil erasevolume HFS+ "$RAMDISK" $(hdiutil attach -nomount ram://$((SIZE*2048)))
 mdutil -i on "/Volumes/$RAMDISK"
-mdutil -i on "/Volumes/XCode Boost"
