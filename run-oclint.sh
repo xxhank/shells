3671de6484ddf428836545432b3b0a6559d3ff5f
王超9 <wangchao9@le.com>
2017-06-17 16:57:08 +0800

    添加调用oclint验证的脚本

--- run-oclint.sh
+++ run-oclint.sh
@@ -1,20 +1,68 @@
 #!/usr/bin/env bash
 
-ROOTDIR="$1"
-CLEAN="${2:-""}"
+ROOTDIR="$1";shift;
+
 WORKSPACE="Le123PhoneClient"
 SCHEME="sdsp"
-pushd "$ROOTDIR"
+
+for i in "$@"
+do
+case $i in
+    --analyze)
+    ANALYZE=YES
+    FIXHTML=YES
+    shift
+    ;;
+
+    --clean)
+    CLEAN=YES
+    shift
+    ;;
+
+    --fix-html)
+    FIXHTML=YES
+    shift
+    ;;
+
+    --build)
+    BUILD=YES
+    FIXHTML=YES
+    ANALYZE=YES
+    shift
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+done
+
 
 XCODEBUILD="xcodebuild -workspace $WORKSPACE.xcworkspace -scheme $SCHEME -configuration Debug -sdk iphoneos"
-if [[ "$CLEAN" == "--clean" ]]; then
+
+pushd "$ROOTDIR"
+if [[ "$CLEAN" == "YES" ]]; then
+    echo "blean..."
     $XCODEBUILD clean 2>&1 | grep -v "Plug-ins"
 fi
 
-# shellcheck disable=
-$XCODEBUILD build 2>&1 | xcpretty -r json-compilation-database | grep -v "Plug-ins"
+if [[ "$BUILD" == "YES" ]]; then
+    echo "build...."
+    # shellcheck disable=
+    $XCODEBUILD build 2>&1 | xcpretty -r json-compilation-database | grep -v "Plug-ins"
+    mv -f ./build/reports/compilation_db.json ./compile_commands.json
+fi
 popd
 
-./shells/oclint/bin/oclint-json-compilation-database ./compile_commands.json
-#inferTraceBugs --html
-#open "./infer-out/report.html/index.html"
+if [[ "$ANALYZE" == "YES" ]]; then
+    echo "analyze...."
+    ./shells/oclint/bin/oclint-json-compilation-database -e sharepods -e Pods -e lib -- -o=lint.html -report-type=html
+fi
+
+if [[ $FIXHTML == "YES" ]]; then
+    if [[ -e ./lint.html ]]; then
+        echo "fix html...."
+        sourceText='<\/style><\/head>'
+        targetText='<\/style><script src=\"jquery-3.2.1.min.js\" type="text\/javascript"><\/script><script src=\"lint-fix.js\" type=\"text\/javascript\"><\/script><\/head>'
+        sed -i .bak -E "s/$sourceText/$targetText/" ./lint.html
+    fi
+fi
